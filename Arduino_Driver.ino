4ce92201c451462a110563da1707569eac2da5d6
slurpeecup <zaidwyasin@gmail.com>
2022-10-06 22:16:22 -0700

    lets go git push

---
+++ Arduino_Driver.ino
@@ -0,0 +1,29 @@
+
+// blink mode:
+/*
+ void setup() {
+  pinMode(LED_BUILTIN, OUTPUT);
+
+}
+
+void loop() 
+{
+digitalWrite(LED_BUILTIN,HIGH);
+delay(100);
+digitalWrite(LED_BUILTIN,LOW);
+delay(100);
+
+}
+*/
+
+// switch control mode
+void setup() {
+  pinMode(LED_BUILTIN, OUTPUT);
+
+}
+
+void loop() 
+{
+digitalWrite(LED_BUILTIN,HIGH);
+
+}
